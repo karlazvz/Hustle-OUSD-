dfb3839fb4d3fc7fb28d1eeb9e365b9bc4334489
karla zuniga <karla@karlas-MacBook-Pro.local>
2025-06-18 17:40:12 -0700

    add lab2

---
+++ lab2.py
@@ -0,0 +1,27 @@
+# Lab 2: Teju Edidi
+
+# Step 1: Name Variable
+name = "Karla Zuniga" 
+
+# Step 2: Nickname Variable
+nickname = "karla"
+
+# Step 3: Age Variable 
+# int = whole num, float = decimal
+age = 17
+# Step 4: Boolean Variable (True or False)
+used_before = False
+
+# Step 5: List Data Structure 
+hobbies = ["read", "gym", "draw", "movies"]
+
+# Step 6: Dictionary (key : value) pairs
+my_favs = {"music" : ["BTS" , "Brent Faiyaz"], "animal" : "kitties"}
+
+# Step 7: Set Data Structure 
+orange = {"alicia", "melanie", "candy", "caliegh", "karla", "karla"}
+print(orange) 
+
+# Step 8: Tuple Variable 
+orange1 = ("alicia", "melanie", "candy", "caliegh", "karla", "karla")
+print(orange1) 
